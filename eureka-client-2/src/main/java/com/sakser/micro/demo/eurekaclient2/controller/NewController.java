bd88004b0f155d765b2a8a82b8d56a6719ca8b38
Orleond <mr.orleond@gmail.com>
2024-09-17 21:09:31 +0300

    Добавлены и настроены все сервисы

--- eureka-client-2/src/main/java/com/sakser/micro/demo/eurekaclient2/controller/NewController.java
+++ eureka-client-2/src/main/java/com/sakser/micro/demo/eurekaclient2/controller/NewController.java
@@ -1,19 +1,15 @@
-package com.sakser.micro.demo.eurekaclient.controller;
+package com.sakser.micro.demo.eurekaclient2.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/main")
-public class TestController {
-
-    @Value("${eureka.instance.instance-id}")
-    private String port;
+@RequestMapping("/new")
+public class NewController {
 
     @GetMapping("/test")
     public String test() {
-        return port;
+        return "new test";
     }
 }
